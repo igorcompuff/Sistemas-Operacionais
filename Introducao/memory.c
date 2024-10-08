96149b1eb4c777b1796c5b4a774e12754dc8dae4
Igor Ribeiro <igornzalez@gmail.com>
2024-10-08 16:42:59 -0300

    Memory updated.

--- Introducao/memory.c
+++ Introducao/memory.c
@@ -2,33 +2,28 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int value;
-
-int soma(int a, int b);
-
 int main (int argc, char *argv[])
-{
-    if (argc != 2) {
-        fprintf(stderr, "usage: mem <value>\n");
-        exit(1);
+{   
+    int *p = malloc(sizeof(int));
+    
+    if (p == NULL) {
+        fprintf(stderr, "Erro ao alocar memoria.");
+        return 1;
     }
     
-    //int *p = malloc(sizeof(int));
-    int *p = &value;
     
-    printf("(pid %d) endereço de p: %llx\n", (int) getpid(), (unsigned long long) &p);
-    printf("(pid %d) endereço armazenado em p: %llx\n", (int) getpid(), (unsigned long long) p);
+    printf("(pid %d) endereço de p: %p\n", (int) getpid(), &p);
+    printf("(pid %d) endereço armazenado em p: %p\n", (int) getpid(), p);
 
-    *p = atoi(argv[1]);
+    *p = 0;
 
     while(1)
     {
         usleep(500* 1000);
         *p = *p + 1;
 
-        printf("(pid %d) Valor de p: %d\n", getpid(), *p);
+        printf("(pid %d) Valor apontado por p: %d\n", getpid(), *p);
     }
 
     return 0;
 }
-
